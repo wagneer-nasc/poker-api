8d3e16e911110d2d867c0a99177351033b40a636
Wagner Nascimento <wagneer.nasc@gmail.com>
2020-11-25 22:19:49 -0300

    add paginacao

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import Header from '../../components/Header';
 import Input from '../../components/Input';
 import {
@@ -11,12 +11,16 @@ import {
     ContainerInput,
     ContainerTextName,
     ContainerTextType,
+    ContainerButtonNextPrevious,
+    ButtonNext,
+    ButtonPrevious,
 
 } from './styles';
 import api from '../../service/api';
 import Axios from 'axios';
-import { Alert } from 'react-native';
+import { Alert, Text, TextInputProps } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Feather';
 
 
 interface Pokemon {
@@ -29,10 +33,7 @@ interface Pokemon {
     }]
 
 }
-interface PokemonData {
-    name: string;
-    url: string;
-}
+
 export interface PokemonList {
     id: string;
     name: string;
@@ -42,31 +43,39 @@ export interface PokemonList {
 
 const Home: React.FC = () => {
     const navigation = useNavigation();
-    const [next, setNext] = useState<string>();
-    const [previous, setPrevious] = useState<string>();
-    const [pokemon, setPokemon] = useState<PokemonData[]>([]);
+    const [next, setNext] = useState<string>('');
+    const [previous, setPrevious] = useState<string>('');
     const [pokemons, setPokemons] = useState<PokemonList[]>([]);
     const [namePokemon, setNamePokemon] = useState<string>('');
+    const [urlPokemonData, setUrlPokemonData] = useState<string>('pokemon?offset=10&limit=10'); 
     const pokemonData: PokemonList[] = ([]);
 
 
     useEffect(() => {
-        async function loadPokemon() {
-
-            await api.get<Pokemon>(`pokemon?limit=5`).then(response => {
-                setNext(response.data.next);
-                setPrevious(response.data.previous);
-                setPokemon(response.data.results);
-
-                pokemon.forEach((item) => {
-                    loadDataPokemon(item.url);
-                })
-            })
+        if (!namePokemon) {
+            loadPokemon(urlPokemonData);
         }
+    }, [namePokemon]);
 
-        loadPokemon();
+    async function nextPokemon() {
+        if (next) loadPokemon(next);
 
-    }, []);
+    }
+    async function previousPokemon() {
+        if (previous) loadPokemon(previous);
+    }
+
+    async function loadPokemon(url: string) {
+        await api.get<Pokemon>(`${url}`).then(response => {
+            setNext(response.data.next);
+            setPrevious(response.data.previous);
+
+            response.data.results.map((item) => {
+                loadDataPokemon(item.url)
+            })
+        })
+
+    }
 
     async function loadDataPokemon(url: string) {
         const types: string[] = [];
@@ -82,10 +91,9 @@ const Home: React.FC = () => {
             }
             pokemonData.push(data);
 
-        }).catch(function (err) {
-            console.log(err)
         })
         setPokemons(pokemonData)
+
     }
 
     async function searchPokemon() {
@@ -106,9 +114,10 @@ const Home: React.FC = () => {
                 Alert.alert('Pokemon não encontrado')
             })
         }
+
     }
     function openDetails(id: string) {
-        navigation.navigate('Details', {id});
+        navigation.navigate('Details', { id });
     }
 
     return (
@@ -122,36 +131,50 @@ const Home: React.FC = () => {
                     onSubmitEditing={() => {
                         searchPokemon();
                     }}
+                    enablesReturnKeyAutomatically={true}
                     value={namePokemon}
-                    onChangeText={setNamePokemon}
+                    onChangeText={setNamePokemon}  
                     placeholder="Type the Pokémon name"
                     name="search"
                     icon="search"
                 ></Input>
             </ContainerInput>
-            <PokemonFlatList
-                data={pokemons}
-                keyExtractor={(pokemon: PokemonList) => pokemon.id.toString()}
-                renderItem={({ item }: { item: PokemonList }) => (
-                    <>
-                        <ContainerListInfo onPress={() => {openDetails(item.id)}}>
-                            <ImageStoreList source={
-                                {
-                                    uri: `https://pokeres.bastionbot.org/images/pokemon/${item.id}.png`
-                                }} />
-                            <ContainerTextName>
-                                <TextLabel> Name: </TextLabel>
-                                <TextName>{item.name}</TextName>
-                            </ContainerTextName>
-                            <ContainerTextType>
-                                <TextLabel>Types: </TextLabel>
-                                <TextName> {item.types[0]}</TextName>
-                                <TextName> , {item.types[1]}</TextName>
-                            </ContainerTextType>
-                        </ContainerListInfo>
-                    </>
-                )}
-            />
+            {pokemons !== null && (
+                <PokemonFlatList
+                    data={pokemons}
+                    keyExtractor={(pokemon: PokemonList) => pokemon.id.toString()}
+                    renderItem={({ item }: { item: PokemonList }) => (
+                        <>
+                            <ContainerListInfo onPress={() => { openDetails(item.id) }}>
+                                <ImageStoreList source={
+                                    {
+                                        uri: `https://pokeres.bastionbot.org/images/pokemon/${item.id}.png`
+                                    }} />
+                                <ContainerTextName>
+                                    <TextLabel> Name: </TextLabel>
+                                    <TextName>{item.name}</TextName>
+                                </ContainerTextName>
+                                <ContainerTextType>
+                                    <TextLabel>Types: </TextLabel>
+                                    <TextName> {item.types[0]}</TextName>
+                                    <TextName> , {item.types[1]}</TextName>
+                                </ContainerTextType>
+                            </ContainerListInfo>
+                        </>
+                    )}
+                />
+            )}
+            <>
+                <ContainerButtonNextPrevious>
+                    <ButtonPrevious onPress={() => { previousPokemon() }}>
+                        <Icon name="chevron-left" size={25} color="#FF9000" />
+                    </ButtonPrevious>
+                    <ButtonNext onPress={() => { nextPokemon() }}>
+                        <Icon name="chevron-right" size={25} color="#FF9000" />
+                    </ButtonNext>
+
+                </ContainerButtonNextPrevious>
+            </>
         </Container>
 
     );
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,10 +1,10 @@
 import styled from 'styled-components/native';
-import { RectButton,FlatList } from 'react-native-gesture-handler';
-import {PokemonList} from './index';
+import { RectButton, FlatList } from 'react-native-gesture-handler';
+import { PokemonList } from './index';
 
 export const Container = styled.View`
 flex: 1;
-`; 
+`;
 
 export const StoreImage = styled.Image`
 width: 56px;
@@ -14,7 +14,7 @@ border-radius: 28px;
 
 export const PokemonFlatList = styled(FlatList as new () => FlatList<PokemonList>)`
  padding: 10px 25px 16px;    
-`; 
+`;
 
 export const ContainerListInfo = styled(RectButton)`
 background: #28262E;
@@ -31,7 +31,7 @@ border-radius: 20px;
 
    
 `;
- 
+
 export const TextoInfo = styled.Text`
 color: #999591;
 font-family: 'RobotoSlab-Medium';
@@ -42,7 +42,7 @@ export const ContainerInput = styled.View`
     padding: 18px;
 `;
 
-export const  ContainerTextName = styled.View`
+export const ContainerTextName = styled.View`
     flex-direction: row;
     padding: 10px;
 `;
@@ -52,7 +52,7 @@ export const ContainerTextType = styled.View`
     flex-direction: row;
     padding: 10px;
 `;
-export const  TextLabel = styled.Text`
+export const TextLabel = styled.Text`
 color: #f4ede8;
 font-family: 'RobotoSlab-Regular';
 font-size: 20px;
@@ -63,10 +63,27 @@ margin-top: 10px;
 export const TextType = styled.Text`
  
 `;
-export const  TextName = styled.Text`
+export const TextName = styled.Text`
 color: #FF9000;
 font-family: 'RobotoSlab-Medium';
 font-size: 20px;
 margin-top: 10px;
  `;
+
+export const ContainerButtonNextPrevious = styled.View`
+    margin-bottom: 10px; 
+    padding: 20px;
+    
+    flex-direction: row; 
+    align-items: center;
+    justify-content: space-around;
+   
+    
+
+ `;
+export const ButtonNext = styled(RectButton)`
+  
+`;
+export const ButtonPrevious = styled(RectButton)`
  
+`;
